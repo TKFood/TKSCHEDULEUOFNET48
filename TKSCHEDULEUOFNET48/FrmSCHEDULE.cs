32a225bb9056958155ecdfa69596917d67869104
at3158 <tk290@tkfood.com.tw>
2024-09-25 09:05:14 +0800

    FORM_AUTO_APPROVAL

--- TKSCHEDULEUOFNET48/FrmSCHEDULE.cs
+++ TKSCHEDULEUOFNET48/FrmSCHEDULE.cs
@@ -77,51 +77,60 @@ namespace TKSCHEDULEUOFNET48
         public void FORM_AUTO_APPROVAL()
         {
 
-            DataTable DT_TASK = SEARCHUOFTB_WKF_TASK();
-
-            //UserUCO useruco = new UserUCO();
-            //EBUser ebuser = useruco.GetEBUser("b6f50a95-17ec-47f2-b842-4ad12512b431");
-
-            string publicKey = "PFJTQUtleVZhbHVlPjxNb2R1bHVzPitNdXJpamQxZ3YzMmZkVzlZUXdBNVNPa3g3bHR4cFUxYlM2UjZaRGU3Y2hXWFpJQVBXMitiRkVacTRUMEIrR3VTVUFkNDl5QnBkVUtFek1Sa1RwcGtaVFlkVGNOeTBJcVc4UVluWWRXNWdNQjRyNitjZGpobTRPamEyaGJybDVYQzdsY3N6cGVDSUg4TzZ1REQ5N0kxdjBUYUlHZkphejFiM2l6Y3h5R1R6VT08L01vZHVsdXM+PEV4cG9uZW50PkFRQUI8L0V4cG9uZW50PjwvUlNBS2V5VmFsdWU+";
-            string NAME = RSAEncrypt(publicKey, "iteng");
-            string PS = RSAEncrypt(publicKey, "P@ssw0rd0");
-
-            Auth.Authentication auth = new Auth.Authentication();
-            auth.Url = "https://eip.tkfood.com.tw/UOF/PublicAPI/System/Authentication.asmx";
-            string TKID = auth.GetToken("IT", NAME, PS);
-            //TKID = "AB536ED0CA9438AD6AB714880A37BF0BC68B866A652467F7";
-            //TKID = "AB536ED0CA9438AD6AB714880A37BF0B2648F42CEE71B039";
-
-
-            BPM.Wkf wkf = new BPM.Wkf();
-            wkf.Url = "https://eip.tkfood.com.tw/UOF/publicapi/wkf/wkf.asmx";
-
-            string token = TKID;
-            string signerGuid = "";
-            string taskId = "";
-            string siteId = "";
-            int nodeSeq = 0;
-            if (DT_TASK != null && DT_TASK.Rows.Count >= 1)
-            {
-                foreach (DataRow DR in DT_TASK.Rows)
-                {
-                    taskId = DR["TASK_ID"].ToString();
-                    siteId = DR["SITE_ID"].ToString();
-                    nodeSeq = Convert.ToInt32(DR["NODE_SEQ"].ToString());
-                    signerGuid= DR["ORIGINAL_SIGNER"].ToString();
-
-                    wkf.SignNext(token, taskId, siteId, nodeSeq, signerGuid);
-                }
-
-            }
-
-
-
             //SignNext(string token, string taskId, string siteId, int nodeSeq, string signerGuid) 
             //public string SignNext2(string token, string taskId, string siteId, int nodeSeq, string signerGuid);
             //wkf.SignNext2(token, taskId, siteId, nodeSeq, signerGuid);
 
-            MessageBox.Show("完成");
+            try
+            {
+                DataTable DT_TASK = SEARCHUOFTB_WKF_TASK();
+
+                //UserUCO useruco = new UserUCO();
+                //EBUser ebuser = useruco.GetEBUser("b6f50a95-17ec-47f2-b842-4ad12512b431");
+
+                string publicKey = "PFJTQUtleVZhbHVlPjxNb2R1bHVzPitNdXJpamQxZ3YzMmZkVzlZUXdBNVNPa3g3bHR4cFUxYlM2UjZaRGU3Y2hXWFpJQVBXMitiRkVacTRUMEIrR3VTVUFkNDl5QnBkVUtFek1Sa1RwcGtaVFlkVGNOeTBJcVc4UVluWWRXNWdNQjRyNitjZGpobTRPamEyaGJybDVYQzdsY3N6cGVDSUg4TzZ1REQ5N0kxdjBUYUlHZkphejFiM2l6Y3h5R1R6VT08L01vZHVsdXM+PEV4cG9uZW50PkFRQUI8L0V4cG9uZW50PjwvUlNBS2V5VmFsdWU+";
+                string NAME = RSAEncrypt(publicKey, "iteng");
+                string PS = RSAEncrypt(publicKey, "P@ssw0rd0");
+
+                Auth.Authentication auth = new Auth.Authentication();
+                auth.Url = "https://eip.tkfood.com.tw/UOF/PublicAPI/System/Authentication.asmx";
+                string TKID = auth.GetToken("IT", NAME, PS);
+                //TKID = "AB536ED0CA9438AD6AB714880A37BF0BC68B866A652467F7";
+                //TKID = "AB536ED0CA9438AD6AB714880A37BF0B2648F42CEE71B039";
+
+
+                BPM.Wkf wkf = new BPM.Wkf();
+                wkf.Url = "https://eip.tkfood.com.tw/UOF/publicapi/wkf/wkf.asmx";
+
+                string token = TKID;
+                string signerGuid = "";
+                string taskId = "";
+                string siteId = "";
+                int nodeSeq = 0;
+                if (DT_TASK != null && DT_TASK.Rows.Count >= 1)
+                {
+                    foreach (DataRow DR in DT_TASK.Rows)
+                    {
+                        taskId = DR["TASK_ID"].ToString();
+                        siteId = DR["SITE_ID"].ToString();
+                        nodeSeq = Convert.ToInt32(DR["NODE_SEQ"].ToString());
+                        signerGuid = DR["ORIGINAL_SIGNER"].ToString();
+
+                        wkf.SignNext(token, taskId, siteId, nodeSeq, signerGuid);
+                    }
+
+                    MessageBox.Show("完成");
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            { }
+            
 
         }
         public DataTable SEARCHUOFTB_WKF_TASK()
